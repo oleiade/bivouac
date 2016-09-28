127d7bb7f14a8916b60daa33056aec3bc4cb4b0a
tcr <tcr@ableton.com>
2016-09-28 09:02:55 +0200

    Cleanup comment.go accord to gometalinter recommendations

--- comment.go
+++ comment.go
@@ -2,11 +2,13 @@ package main
 
 import "time"
 
+// Comment represents a comment added to an issue
 type Comment struct {
 	CreatedAt time.Time
 	Body      string
 }
 
+// NewComment creates a new comment instance
 func NewComment(createdAt time.Time, body string) *Comment {
 	return &Comment{
 		CreatedAt: createdAt,
